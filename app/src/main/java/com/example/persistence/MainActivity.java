e26ec31e1ab5935c91604d3fd5af05af79385611
Pistolmannen <erikgustafsson@gmail.com>
2024-05-06 15:42:37 +0200

    SQLite added

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.persistence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SQLiteDatabase database;
+    private DatabaseHelper databaseHelper;
+    private Button saveButton;
+    private Button writeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseHelper = new DatabaseHelper(this);
+        database = databaseHelper.getWritableDatabase();
+
+        //saveButton.findViewById(R.id.saveButton);
+        //writeButton.findViewById(R.id.writeButton);
+
+
     }
 }
